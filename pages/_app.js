e4b99059c104b6896ce912b0b9acad1cf27c34c4
oomaar <omaar_5@hotmail.com>
2022-05-07 16:59:51 +0200

    encapsulate in a ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import { GlobalStyle } from "../global/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, theme } from "../global/GlobalStyle";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
