d9b99587a4338330531d89a7db0d9435a823343e
oomaar <omaar_5@hotmail.com>
2022-05-08 01:57:16 +0200

    complete Landing Contaier

---
+++ containers/LandingContainer.js
@@ -0,0 +1,11 @@
+import { Footer, Hero, Navbar } from "../components/LandingPage";
+
+export const LandingContainer = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
+};
