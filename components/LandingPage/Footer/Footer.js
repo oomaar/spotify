508fc943586e81890cd042a10b7de0de03cdcf48
oomaar <omaar_5@hotmail.com>
2022-05-07 22:45:07 +0200

    complete Footer Top

--- components/LandingPage/Footer/Footer.js
+++ components/LandingPage/Footer/Footer.js
@@ -1,5 +1,78 @@
-import { FooterContainer } from "./styledFooter";
+import { NavLogo } from "../Navbar/styledNavbar";
+import {
+  FooterContainer,
+  FooterLinksContaier,
+  FooterLinksList,
+  FooterLink,
+  FooterTop,
+  FooterSocialContaier,
+  FooterSocialIcon,
+} from "./styledFooter";
 
 export const Footer = () => {
-  return <FooterContainer>Footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      <FooterTop>
+        <NavLogo>
+          <i className="bx bxl-spotify" />
+          <p>Spotify</p>
+        </NavLogo>
+        <FooterLinksContaier>
+          <FooterLinksList>
+            <p>Company</p>
+            <li>
+              <FooterLink>About</FooterLink>
+            </li>
+            <li>
+              <FooterLink>Jobs</FooterLink>
+            </li>
+            <li>
+              <FooterLink>For the Record</FooterLink>
+            </li>
+          </FooterLinksList>
+          <FooterLinksList>
+            <p>Communities</p>
+            <li>
+              <FooterLink>For Artists</FooterLink>
+            </li>
+            <li>
+              <FooterLink>Developers</FooterLink>
+            </li>
+            <li>
+              <FooterLink>Advertising</FooterLink>
+            </li>
+            <li>
+              <FooterLink>Investors</FooterLink>
+            </li>
+            <li>
+              <FooterLink>Vendors</FooterLink>
+            </li>
+          </FooterLinksList>
+          <FooterLinksList>
+            <p>Useful Links</p>
+            <li>
+              <FooterLink>Support</FooterLink>
+            </li>
+            <li>
+              <FooterLink>Web Player</FooterLink>
+            </li>
+            <li>
+              <FooterLink>Free Mobile App</FooterLink>
+            </li>
+          </FooterLinksList>
+        </FooterLinksContaier>
+        <FooterSocialContaier>
+          <FooterSocialIcon>
+            <i className="bx bxl-instagram" />
+          </FooterSocialIcon>
+          <FooterSocialIcon>
+            <i className="bx bxl-twitter" />
+          </FooterSocialIcon>
+          <FooterSocialIcon>
+            <i className="bx bxl-facebook" />
+          </FooterSocialIcon>
+        </FooterSocialContaier>
+      </FooterTop>
+    </FooterContainer>
+  );
 };
--- components/LandingPage/Footer/styledFooter.js
+++ components/LandingPage/Footer/styledFooter.js
@@ -1,3 +1,104 @@
 import styled from "styled-components";
+import {
+  lgScreen,
+  transitionFast,
+  xlScreen,
+} from "../../../global/GlobalStyle";
 
-export const FooterContainer = styled.footer``;
+export const FooterContainer = styled.footer`
+  background: #000;
+  color: #fff;
+  padding: 2rem 10rem 0.5rem;
+
+  @media screen and (max-width: 1100px) {
+    padding: 2rem 8rem 0.5rem;
+  }
+
+  @media screen and (max-width: ${lgScreen}) {
+    padding: 2rem 1rem 0.5rem;
+  }
+`;
+
+export const FooterTop = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  @media screen and (max-width: ${xlScreen}) {
+    flex-direction: column;
+  }
+`;
+
+export const FooterLinksContaier = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  @media screen and (max-width: ${xlScreen}) {
+    margin: 2rem 0;
+  }
+
+  @media screen and (max-width: ${lgScreen}) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+export const FooterLinksList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0 2rem;
+
+  p {
+    color: ${({ theme }) => theme.colors.textColor};
+    text-transform: uppercase;
+    font-weight: ${({ theme }) => theme.weight.medium};
+  }
+
+  li {
+    margin: 1rem 0;
+  }
+
+  @media screen and (max-width: ${xlScreen}) {
+    :first-child {
+      margin-left: 0;
+    }
+  }
+
+  @media screen and (max-width: ${lgScreen}) {
+    margin-left: 0;
+  }
+`;
+
+export const FooterLink = styled.a`
+  color: #fff;
+  font-size: 1.2rem;
+  cursor: pointer;
+
+  :hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export const FooterSocialContaier = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  @media screen and (max-width: ${xlScreen}) {
+    margin-top: 10rem;
+  }
+`;
+
+export const FooterSocialIcon = styled.div`
+  background-color: ${({ theme }) => theme.colors.containerColor};
+  border-radius: 100%;
+  padding: 1rem;
+  font-size: 2rem;
+  cursor: pointer;
+
+  i {
+    transition: ${transitionFast};
+  }
+
+  :hover i {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
