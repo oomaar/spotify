5af89393e79b7abaf33846be5557435d8347e5a2
oomaar <omaar_5@hotmail.com>
2022-05-07 22:06:15 +0200

    complete Hero Component Responsive

--- components/LandingPage/Hero/Hero.js
+++ components/LandingPage/Hero/Hero.js
@@ -1,5 +1,20 @@
-import { HeroContainer } from "./styledHero";
+import {
+  HeroButton,
+  HeroContainer,
+  HeroSubtitle,
+  HeroTitle,
+} from "./styledHero";
 
 export const Hero = () => {
-  return <HeroContainer>Hero</HeroContainer>;
+  return (
+    <HeroContainer>
+      <HeroTitle>
+        Listening is <div>everything</div>
+      </HeroTitle>
+      <HeroSubtitle>
+        Millions of songs and podcasts. No credit card needed.
+      </HeroSubtitle>
+      <HeroButton>Get Spotify Free</HeroButton>
+    </HeroContainer>
+  );
 };
