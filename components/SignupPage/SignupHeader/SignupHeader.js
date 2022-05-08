a767392586721fadb81fd565299e4fe772302569
oomaar <omaar_5@hotmail.com>
2022-05-08 15:40:16 +0200

    complete Signup Header

--- components/SignupPage/SignupHeader/SignupHeader.js
+++ components/SignupPage/SignupHeader/SignupHeader.js
@@ -1,7 +1,13 @@
-import { SignupHeaderContainer } from "./styledSignupHeader"
+import { SignupHeaderContainer, SignupHeaderLogo, SignupHeaderText } from "./styledSignupHeader"
 
 export const SignupHeader = () => {
     return (
-        <SignupHeaderContainer>SignupHeader</SignupHeaderContainer>
+        <SignupHeaderContainer>
+            <SignupHeaderLogo>
+                <i className="bx bxl-spotify" />
+                <p>Spotify</p>
+            </SignupHeaderLogo>
+            <SignupHeaderText>Sign up for free to start listening.</SignupHeaderText>
+        </SignupHeaderContainer>
     )
 }
