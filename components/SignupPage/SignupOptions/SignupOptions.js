ed8f28d6359447cf04d7efff7bb6445ab17092ce
oomaar <omaar_5@hotmail.com>
2022-05-11 23:02:04 +0200

    prettier refactor

--- components/SignupPage/SignupOptions/SignupOptions.js
+++ components/SignupPage/SignupOptions/SignupOptions.js
@@ -1,17 +1,23 @@
-import { SignupOptionsButton, SignupOptionsContainer, SignupOptionsFooter } from "./styledSignupOptions"
+import {
+  SignupOptionsButton,
+  SignupOptionsContainer,
+  SignupOptionsFooter,
+} from "./styledSignupOptions";
 
 export const SignupOptions = () => {
-    return (
-        <SignupOptionsContainer>
-            <SignupOptionsButton facebook>
-                <i className='bx bxl-facebook-circle' />
-                Sign up with Facebook
-            </SignupOptionsButton>
-            <SignupOptionsButton>
-                <i className='bx bxl-google' />
-                Sign up with Google
-            </SignupOptionsButton>
-            <SignupOptionsFooter><span>or</span></SignupOptionsFooter>
-        </SignupOptionsContainer>
-    )
-}
+  return (
+    <SignupOptionsContainer>
+      <SignupOptionsButton facebook>
+        <i className="bx bxl-facebook-circle" />
+        Sign up with Facebook
+      </SignupOptionsButton>
+      <SignupOptionsButton>
+        <i className="bx bxl-google" />
+        Sign up with Google
+      </SignupOptionsButton>
+      <SignupOptionsFooter>
+        <span>or</span>
+      </SignupOptionsFooter>
+    </SignupOptionsContainer>
+  );
+};
