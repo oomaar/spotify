9189f7cb3768e52651ff4bfb1a86169a496f0c09
oomaar <omaar_5@hotmail.com>
2022-05-21 16:34:28 +0200

    [WIP] Request failed with error 401 [user token]

--- pages/browse.js
+++ pages/browse.js
@@ -7,7 +7,8 @@ import {
   getDocs,
   doc,
 } from "firebase/firestore";
-import { getSession, signOut, useSession } from "next-auth/react";
+import { getToken } from "next-auth/jwt";
+import { getCsrfToken, getSession, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { db } from "../firebase.config";
@@ -16,14 +17,26 @@ const Browse = () => {
   // This screen supposed to be Private
   const { data: session, loading } = useSession();
   const router = useRouter();
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+  // const [userToken, setUserToken] = useState();
+  // console.log("🚀 ~ file: browse.js ~ line 21 ~ Browse ~ userToken", userToken);
 
+  // async function getCosOmToken() {
+  //   const csrfToken = await getCsrfToken();
+  //   setUserToken(csrfToken);
+  // }
+
+  // getCosOmToken();
   // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await axios.get(`https://api.spotify.com/v1/tracks`);
-  //     // { { headers: {"Authorization" : `Bearer ${token}`} } })
-  //   };
+  //   const fetchData = async () =>
+  //     await axios
+  //       .get(`https://api.spotify.com/v1/tracks`, {
+  //         headers: { Authorization: `Bearer ${userToken}` },
+  //       })
+  //       .then((res) => setData(res.data));
+  //   fetchData();
   // }, []);
+  // console.log("data", data);
 
   useEffect(() => {
     if (!session && !loading) {
@@ -45,8 +58,15 @@ const Browse = () => {
 export default Browse;
 
 export async function getServerSideProps(context) {
-  const { res } = context;
+  const { req, res } = context;
   const session = await getSession(context);
+
+  // if using `NEXTAUTH_SECRET` env variable, we detect it, and you won't actually need to `secret`
+  // const token = await getToken({ req })
+  const secret =
+    "asjfd8u23qifheg78ry23d3786823ry87gf732t89ydui32t8698ryd9749368ydhu9r8g78598y9ry6019fy38";
+  const token = await getToken({ req, secret });
+  console.log("JSON Web Token", token);
 
   if (!session) {
     res.statusCode = 302;
