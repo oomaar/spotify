f8d502cf2486087468752ff4bd417e062d09dd5e
oomaar <omaar_5@hotmail.com>
2022-07-23 17:51:55 +0200

    setup browse screen

--- pages/browse.js
+++ pages/browse.js
@@ -1,9 +1,11 @@
 const Browse = () => {
   return (
     <div>
-      <h1>browse screen</h1>
-
-      <button>Sign Out</button>
+      <main>
+        {/* Sidebar on the left */}
+        {/* Center browse component */}
+      </main>
+      <div>{/* Player at the bottom */}</div>
     </div>
   );
 };
