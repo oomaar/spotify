aa69240a7a27c4b2316dd8d642d65771edae7a60
oomaar <omaar_5@hotmail.com>
2022-08-11 20:05:48 +0200

    add some notes

---
+++ Notes/Notes.js
@@ -0,0 +1,13 @@
+// Tools Used
+// Nextjs 12
+// React
+// Debounce
+// Spotify api
+// JWT oauth tokens (Access & refresh Tokens) & authenticating users
+// Nextauth
+// Recoil
+
+// Pause: 12:03 23/7/2022
+// Pause 2: 42:11 23/7/2022
+
+// Register on spotify & Create Playlists in order to use The API
