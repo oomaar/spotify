0be50f10925e92eb5b534511553fcc4835ed6665
oomaar <omaar_5@hotmail.com>
2022-08-12 19:02:42 +0200

    update Notes

--- Notes/Notes.js
+++ Notes/Notes.js
@@ -12,3 +12,6 @@
 
 // Register on spotify & Create Playlists in order to use The API
 // Spotify account => login with facebook
+
+// Pause: 51:55 12/8/2022
+// Next: we want those values NEXT_PUBLIC_CLIENT_SECRET NEXT_PUBLIC_CLIENT_ID
---
+++ pages/api/[...nextauth].js
@@ -0,0 +1,8 @@
+// import NextAuth from "next-auth";
+// import SpotifyProvider from "next-auth/providers/spotify";
+
+// export default NextAuth({
+//     SpotifyProvider({
+
+//     })
+// });
