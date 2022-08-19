83d16a6e87031a942b72afb14fbec93eda1c0092
oomaar <omaar_5@hotmail.com>
2022-08-19 22:10:39 +0200

    setup login providers [faceing: an error of null]

--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,40 @@
 import { LoginForm, LoginPageHeader } from "../components/LoginPage";
 import { NextHead } from "../global/NextHead";
 import styled from "styled-components";
+import { getProviders, signIn } from "next-auth/react";
 
 const PageContainer = styled.main`
   display: flex;
   justify-content: center;
 `;
 
-const Login = () => {
+const Login = ({ providers }) => {
+  console.log("🚀 ~ file: login.js ~ line 12 ~ Login ~ providers", providers);
+
   return (
     <div>
       <NextHead title="Login - Spotify" />
       <LoginPageHeader />
 
       <PageContainer>
-        <div>{/* <LoginForm /> */}</div>
+        {/* {Object.values(providers).map((provider) => (
+          <div>
+            <button>Login with {provider.name}</button>
+          </div>
+        ))} */}
       </PageContainer>
     </div>
   );
 };
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
