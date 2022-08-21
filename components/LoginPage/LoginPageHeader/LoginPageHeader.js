d9304bb5a279e027963db1158777f34903051f3b
oomaar <omaar_5@hotmail.com>
2022-08-21 19:26:29 +0200

    re-style login page header

--- components/LoginPage/LoginPageHeader/LoginPageHeader.js
+++ components/LoginPage/LoginPageHeader/LoginPageHeader.js
@@ -1,15 +1,10 @@
-import {
-  LoginPageHeaderContainer,
-  LoginPageHeaderLogo,
-} from "./styledLoginPageHeader";
+import { LoginPageHeaderContainer } from "./styledLoginPageHeader";
 
 export const LoginPageHeader = () => {
   return (
     <LoginPageHeaderContainer>
-      <LoginPageHeaderLogo>
-        <i className="bx bxl-spotify" />
-        <p>Spotify</p>
-      </LoginPageHeaderLogo>
+      <i className="bx bxl-spotify" />
+      <p>Spotify</p>
     </LoginPageHeaderContainer>
   );
 };
--- components/LoginPage/LoginPageHeader/styledLoginPageHeader.js
+++ components/LoginPage/LoginPageHeader/styledLoginPageHeader.js
@@ -1,12 +1,11 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const LoginPageHeaderContainer = styled.div``;
-
-export const LoginPageHeaderLogo = styled.div`
+export const LoginPageHeaderContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: ${({ theme }) => theme.font.mdFont};
   font-weight: ${({ theme }) => theme.weight.bold};
-  border-bottom: 1px solid ${({ theme }) => theme.colors.textColor};
+  color: #fff;
+  box-shadow: 0px 5px 9px rgba(255, 255, 255, 0.2);
 `;
