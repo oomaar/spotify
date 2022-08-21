4bd4db7157dfb689b44bc04a05d34ba6b893cf5f
oomaar <omaar_5@hotmail.com>
2022-08-22 00:37:07 +0200

    wrap app <SessionProvider>

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, theme } from "../global/GlobalStyle";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
